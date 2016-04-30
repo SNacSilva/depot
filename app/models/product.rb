5aacde8cfbcd6b3d684289cd238184a32c3d62b3
Samantha Silva <samanthasilva.n@gmail.com>
2016-04-30 16:24:34 -0300

    Store

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,5 +11,24 @@
 #
 
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :check_if_referenced_by_line_items
+
   validates :title, :description, presence: true
+  validates :price, numericality: {
+      greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
+  def check_if_referenced_by_line_items
+    if self.line_items.empty?
+      true
+    else
+      errors.add(:base, "Há items relacionados, não pode ser removido!")
+      false
+    end
+  end
 end
